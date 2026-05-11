0b19325a02d946f8257d0db4a46e0e2df04c91a2
rmolugu@pgsnsofttech.com <rohangoud0999@gmail.com>
2026-05-11 22:22:50 +0530

    Added warning and critical alerts

--- monitor.tf
+++ monitor.tf
@@ -1,79 +1,10 @@
-resource "azurerm_monitor_action_group" "rohan_action_group" {
-  name                = "rohan-action-group-existingvm-v2"
-  resource_group_name = data.azurerm_resource_group.rohan_rg.name
-  short_name          = "rohanag"
-
-  email_receiver {
-    name          = "send-email"
-    email_address = "rohangoud0999@gmail.com"
-  }
+data "azurerm_resource_group" "rohan_rg" {
+  name = "Rohan-rg"
 }
 
-resource "azurerm_monitor_metric_alert" "cpu_alert" {
-  name                = "high-cpu-alert-existingvm-v2"
+data "azurerm_resources" "existing_vm" {
   resource_group_name = data.azurerm_resource_group.rohan_rg.name
-  scopes              = [data.azurerm_resources.existing_vm.resources[0].id]
 
-  description = "CPU usage alert"
-  severity    = 2
-  frequency   = "PT1M"
-  window_size = "PT5M"
-
-  criteria {
-    metric_namespace = "Microsoft.Compute/virtualMachines"
-    metric_name      = "Percentage CPU"
-    aggregation      = "Average"
-    operator         = "GreaterThan"
-    threshold        = 80
-  }
-
-  action {
-    action_group_id = azurerm_monitor_action_group.rohan_action_group.id
-  }
-}
-
-resource "azurerm_monitor_metric_alert" "memory_alert" {
-  name                = "high-memory-alert-existingvm-v2"
-  resource_group_name = data.azurerm_resource_group.rohan_rg.name
-  scopes              = [data.azurerm_resources.existing_vm.resources[0].id]
-
-  description = "Memory usage alert"
-  severity    = 2
-  frequency   = "PT1M"
-  window_size = "PT5M"
-
-  criteria {
-    metric_namespace = "Microsoft.Compute/virtualMachines"
-    metric_name      = "Available Memory Bytes"
-    aggregation      = "Average"
-    operator         = "LessThan"
-    threshold        = 200000000
-  }
-
-  action {
-    action_group_id = azurerm_monitor_action_group.rohan_action_group.id
-  }
-}
-
-resource "azurerm_monitor_metric_alert" "disk_alert" {
-  name                = "high-disk-alert-existingvm-v2"
-  resource_group_name = data.azurerm_resource_group.rohan_rg.name
-  scopes              = [data.azurerm_resources.existing_vm.resources[0].id]
-
-  description = "Disk usage alert"
-  severity    = 2
-  frequency   = "PT1M"
-  window_size = "PT5M"
-
-  criteria {
-    metric_namespace = "Microsoft.Compute/virtualMachines"
-    metric_name      = "OS Disk Queue Depth"
-    aggregation      = "Average"
-    operator         = "GreaterThan"
-    threshold        = 5
-  }
-
-  action {
-    action_group_id = azurerm_monitor_action_group.rohan_action_group.id
-  }
+  type = "Microsoft.Compute/virtualMachines"
+  name = "rohan-vm-new"
 }
